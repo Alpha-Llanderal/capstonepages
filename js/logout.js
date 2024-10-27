614d2c9f7b51f5be5e98319daddfced4bc1bcb11
Alpha <alpha.kodego@gmail.com>
2024-10-28 06:44:09 +0800

    final

--- js/logout.js
+++
@@ -1,7 +0,0 @@
-function logout() {
-    // Perform logout actions here
-    alert('You have been logged out successfully.');
-    window.location.href = 'login.html'; // Redirect to the login page
-}
-
-// for the logout function from main
